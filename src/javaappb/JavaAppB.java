b7ad1e7b320c954e532b647efa74dae7aae8abed
MJBoss <bustamantemichaeljohn@gmail.com>
2024-08-15 09:52:17 +0800

    my first repo

---
+++ src/javaappb/JavaAppB.java
@@ -0,0 +1,14 @@
+package javaappb;
+
+import java.util.Scanner;
+
+
+
+public class JavaAppB {
+    public static void main(String[] args) {
+        
+        Receipt rcpt = new Receipt();
+        rcpt.getReciept();
+        
+    }  
+}
